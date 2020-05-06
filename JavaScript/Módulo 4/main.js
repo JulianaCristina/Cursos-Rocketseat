0c7afe7cba983437120ec1f3025352a4657a2efc
juliana <julianacristina780@gmail.com>
2020-05-06 19:03:06 -0300

    Promises

--- JavaScript/Módulo 4/main.js
+++ JavaScript/Módulo 4/main.js
@@ -1,10 +1,27 @@
-var xhr = new XMLHttpRequest();
 
-xhr.open("GET", 'http:api.github.com/users/JulianaCristina')
-xhr.send(null)
+var minhaPromise = function () {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", 'http:api.github.com/users/JulianaCristina')
+        xhr.send(null)
 
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText))
-    }
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4){
+                if (xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText))
+                }else{
+                    reject("Erro na requisição")
+                }
+            }
+        }
+    })
 }
+
+
+minhaPromise()
+    .then(function (response) {
+        console.log(response)
+    })
+    .catch(function (error) {
+        console.warn(error)
+    })
