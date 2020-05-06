e776cf610f52c436ada1bef82b714e454b0663e7
juliana <julianacristina780@gmail.com>
2020-05-06 18:33:21 -0300

    Exercises from modulo 3 - Todo List - Saving on local storage

--- JavaScript/Módulo 3/todo.js
+++ JavaScript/Módulo 3/todo.js
@@ -2,11 +2,8 @@ var listElement = document.querySelector("#app ul")
 var inputElement = document.querySelector("#app input")
 var buttonElement = document.querySelector("#app button")
 
-var todos = [
-    'Fazer café',
-    'Estudar Js',
-    'Acessar comunidade'
-]
+var todos = JSON.parse(localStorage.getItem('list_todos')) || []
+
 
 function renderTodo() {
 
@@ -15,7 +12,17 @@ function renderTodo() {
         var todoElement = document.createElement('li')
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement('a')
+        linkElement.setAttribute('href', '#')
+
+        var pos = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', 'deleteTodo('+pos+')')
+
+        var linkText = document.createTextNode('Excluir')
+        linkElement.appendChild(linkText)
+
         todoElement.appendChild(todoText)
+        todoElement.appendChild(linkElement)
         listElement.appendChild(todoElement)
     }
 }
@@ -28,6 +35,19 @@ function addTodo() {
     todos.push(todoText);
     inputElement.value = '';
     renderTodo();
+    saveToStorage()
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos,1)
+    renderTodo()
+    saveToStorage()
+}
+
+function saveToStorage() {
+
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+
+}
