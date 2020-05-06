015a9b385022daa5525badb626ec13d46a1d68a5
juliana <julianacristina780@gmail.com>
2020-05-06 19:22:01 -0300

    Axios

--- JavaScript/Módulo 4/main.js
+++ JavaScript/Módulo 4/main.js
@@ -1,24 +1,31 @@
+//
+// var minhaPromise = function () {
+//     return new Promise(function (resolve, reject) {
+//         var xhr = new XMLHttpRequest();
+//         xhr.open("GET", 'http:api.github.com/users/JulianaCristina')
+//         xhr.send(null)
+//
+//         xhr.onreadystatechange = function () {
+//             if (xhr.readyState === 4){
+//                 if (xhr.status === 200){
+//                     resolve(JSON.parse(xhr.responseText))
+//                 }else{
+//                     reject("Erro na requisição")
+//                 }
+//             }
+//         }
+//     })
+// }
+// minhaPromise()
+//     .then(function (response) {
+//         console.log(response)
+//     })
+//     .catch(function (error) {
+//         console.warn(error)
+//     })
 
-var minhaPromise = function () {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", 'http:api.github.com/users/JulianaCristina')
-        xhr.send(null)
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4){
-                if (xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText))
-                }else{
-                    reject("Erro na requisição")
-                }
-            }
-        }
-    })
-}
-
-
-minhaPromise()
+axios.get('http:api.github.com/users/JulianaCristina')
     .then(function (response) {
         console.log(response)
     })
