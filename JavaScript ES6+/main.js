0012a98b53a8415bdb8bce863e7e2e2d58038b89
juliana <julianacristina780@gmail.com>
2020-05-07 16:55:13 -0300

    ES6- Spread e rest

--- JavaScript ES6+/main.js
+++ JavaScript ES6+/main.js
@@ -99,3 +99,50 @@ function mostraNome({nome, idade}) {
 }
 
 mostraNome(usuario)
+
+
+// rest
+
+const arr = [1,2,3,4]
+
+const[a,b, ...c] = arr;
+
+//exemplo 2
+
+const usuario = {
+    nome: 'Juliana',
+    idade: 22,
+    empresa: 'No one'
+}
+
+const {nome, ...resto} = usuario;
+
+//parametro de funcao
+
+function soma(a, b) {
+    return a + b
+
+}
+
+//to
+
+function soma(...params) {
+    return params.reduce((total, next) => total + next);
+}
+
+//spred - repassa as info p outra estrutura de dados.
+
+const arr1 = [1,2,3]
+const arr2 = [4,5,6]
+
+const arr3 = [ ...arr1, ...arr2]
+
+//sobrepoe apenas uma informação, mantendo as outras
+
+const usuario1= {
+    nome: 'Juliana',
+    idade: 22,
+    empresa: 'No one'
+}
+
+const usuario2 = { ...usuario1, nome:'César'}
