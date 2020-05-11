5c1986ddb706d5822be37696c25bff687bec887a
juliana <julianacristina780@gmail.com>
2020-05-11 11:03:06 -0300

    ES6- Módulo 2 - Webpack - import e export

--- JavaScript ES6+/bundle.js
+++ JavaScript ES6+/bundle.js
@@ -1,165 +1,113 @@
-// class List{
-//     constructor() {
-//         this.data = []
-//     }
-//     add(data){
-//         this.data.push(data)
-//         console.log(this.data)
-//     }
-// }
-//
-// class TodoList extends List{
-//
-//     constructor() {
-//         super();
-//
-//         this.usuario = 'Diego';
-//     }
-//
-//     mostraUsuario(){
-//         console.log(this.usuario)
-//     }
-//
-// }
-//
-// let MinhaLista =  new TodoList();
-//
-// document.getElementById('novotodo').onclick = function () {
-//     MinhaLista.add('NovoTodo');
-// }
-//
-// const arr = [1,2,3,4,5]
-//
-// const newArr = arr.map(function (item, index) {
-//     return item + index;
-// })
-//
-// console.log(newArr)
-//
-// const sum = arr.reduce(function (total, next) {
-//     return total + next
-// })
-//
-// const filter = arr.filter(function (item) {
-//     return item % 2 === 0
-// })
-//
-// const find = arr.find(function (item) {
-//     return item === 4
-// })
-//
-// //__________________________________
-// // Arrow Function
-//
-// const arr = [1,2,3,4,5]
-//
-// const newArr = arr.map((item, index) => {
-//     return item + index;
-// })
-// //quando tem apenas um argumento pode remover os parenteses
-// const newArr = arr.map(item => {
-//     return item + index;
-// })
-// //quando apenas retorna algo, pode tirar o return
-// const newArr = arr.map(item => item + index)
-//
-// //para retornar um objeto de forma simplificada, é necessário colocá-lo dentro de ( )
-// const teste = () => ({nome: 'Juliana'})
-//
-//
-// //__________________________________________________________________
-// //valores padrão
-// //Os valores padrões evita que a funcção retorne NaN quando algum argumento não é passado.
-// const soma = (a = 3, b = 6) => a+b;
-//
-// console.log(soma(1))
-// console.log(soma())
-//
-// //Desestruração;
-//
-// const usuario = {
-//     nome: 'Diego',
-//     idade: 23,
-//     endereco: {
-//         cidade: 'Berlândia',
-//         estado: 'MG'
-//     }
-// }
-// //para evitar fazer assim
-// const nome = usuario.idade;
-// //faz assim
-//
-// const {nome, idade, endereco:{cidade}} = usuario;
-//
-// //Também é possível usar a desestruturação em funções;
-//
-// function mostraNome({nome, idade}) {
-//     console.log(nome, idade)
-// }
-//
-// mostraNome(usuario)
-//
-//
-// // rest
-//
-// const arr = [1,2,3,4]
-//
-// const[a,b, ...c] = arr;
-//
-// //exemplo 2
-//
-// const usuario = {
-//     nome: 'Juliana',
-//     idade: 22,
-//     empresa: 'No one'
-// }
-//
-// const {nome, ...resto} = usuario;
-//
-// //parametro de funcao
-//
-// function soma(a, b) {
-//     return a + b
-//
-// }
-//
-// //to
-//
-// function soma(...params) {
-//     return params.reduce((total, next) => total + next);
-// }
-//
-// //spred - repassa as info p outra estrutura de dados.
-//
-// const arr1 = [1,2,3]
-// const arr2 = [4,5,6]
-//
-// const arr3 = [ ...arr1, ...arr2]
-//
-// //sobrepoe apenas uma informação, mantendo as outras
-//
-// const usuario1= {
-//     nome: 'Juliana',
-//     idade: 22,
-//     empresa: 'No one'
-// }
-//
-// const usuario2 = { ...usuario1, nome:'César'}
-//
-//
-// // template literals
-//
-// // `${variavel}`
-//
-//
-// //Object short syntax
-//
-// const nome = 'Juliana'
-// const idade = 22
-//
-// const usuario = {
-//     nome,
-//     idade,
-//     empresa: 'No one'
-// }
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./main.js":
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
 "use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _soma__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./soma */ \"./soma.js\");\n// import {soma, sub} from'./funcoes';\n//import quando tenho um default\n// import soma, {sub} from'./funcoes';\n\nconsole.log(Object(_soma__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(1, 2)); // console.log(sub(1,2))\n//renomeando\n// import {soma as soma1, sub} from'./funcoes';\n//para importar todas as funcoes\n//import * as funcoes from './funcoes'\n//para acessar as funcoes\n//funcoes.soma\n\n//# sourceURL=webpack:///./main.js?");
+
+/***/ }),
+
+/***/ "./soma.js":
+/*!*****************!*\
+  !*** ./soma.js ***!
+  \*****************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return soma; });\nfunction soma(a, b) {\n  return a + b;\n}\n\n//# sourceURL=webpack:///./soma.js?");
+
+/***/ })
+
+/******/ });
---
+++ JavaScript ES6+/funcoes.js
@@ -0,0 +1,9 @@
+export default function soma(a, b) {
+    return a + b
+}
+
+export function sub(a, b) {
+    return a - b
+}
+
+//posso ter um default
