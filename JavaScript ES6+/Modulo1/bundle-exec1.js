93249af0b0e677648c01d70ee0ebf42d51ef4dbd
juliana <julianacristina780@gmail.com>
2020-05-11 10:11:01 -0300

    ES6- Módulo 1 - exercicio 5

--- JavaScript ES6+/Modulo1/bundle-exec1.js
+++ JavaScript ES6+/Modulo1/bundle-exec1.js
@@ -1,44 +1,61 @@
 "use strict";
 
-// 4.1
-// Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis, no
-// fim deve ser possível fazer o seguinte:
-var usuario = {
-  nome: 'Rocketseat',
-  endereco: {
-    cidade: 'Rio do Sul',
-    estado: 'SC'
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// Utilizando o operador de rest/spread (...) realize as seguintes operações:
+//5.1 Rest
+// A partir do array: const arr = [1, 2, 3, 4, 5, 6], defina uma variável x que recebe a primeira
+// posição do vetor e outra variável y que recebe todo restante dos dados.
+var arr = [1, 2, 3, 4, 5, 6];
+var x = arr[0],
+    y = arr.slice(1);
+console.log(x); // 1
+
+console.log(y); // [2, 3, 4, 5, 6]
+// Crie uma função que recebe inúmeros parâmetros e retorna a soma de todos eles:
+// function soma...
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
   }
-}; //resposta
 
-var nome = usuario.nome,
-    _usuario$endereco = usuario.endereco,
-    cidade = _usuario$endereco.cidade,
-    estado = _usuario$endereco.estado;
-console.log(nome); // Rocketseat
-
-console.log(cidade); // Rio do Sul
-
-console.log(estado); // SC
-// 4.2...
-// Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis, no
-// fim deve ser possível fazer o seguinte:
-//Utilize a desestruturação nos parâmetros da função para buscar o nome e idade do usuário
-// separadamente e a função poder retornar apenas:
-// return `${nome} tem ${idade} anos.`;
-//
-// function mostraInfo(usuario) {
-//     return `${usuario.nome} tem ${usuario.idade} anos.`;
-// }
-// mostraInfo({ nome: 'Diego', idade: 23 })
-
-function mostraInfo(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  console.log("".concat(nome, " tem ").concat(idade, " anos."));
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 }
 
-mostraInfo({
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+
+console.log(soma(1, 2)); // 3
+// 5.2 Spread
+// A partir do objeto e utilizando o operador spread:
+
+var usuario = {
   nome: 'Diego',
-  idade: 23
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil'
+  }
+}; // Crie uma variável usuario2 que contenha todos os dados do usuário porém com nome Gabriel.
+// Crie uma variável usuario3 que contenha todos os dados do usuário porém com cidade Lontras.
+
+var usuario2 = _objectSpread(_objectSpread({}, usuario), {}, {
+  nome: 'Gabriel'
 });
+
+console.log(usuario2);
+
+var usuario3 = _objectSpread(_objectSpread({}, usuario), {}, {
+  endereco: {
+    cidade: 'Lontras'
+  }
+});
+
+console.log(usuario3);
