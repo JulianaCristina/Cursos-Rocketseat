c42c7bd792a38840298ea35920758729d3ae91ea
juliana <julianacristina780@gmail.com>
2020-05-11 10:01:52 -0300

    ES6- Módulo 1 - exercicio 4

--- JavaScript ES6+/Modulo1/exercicio4.js
+++ JavaScript ES6+/Modulo1/exercicio4.js
@@ -1,44 +1,38 @@
-"use strict";
-
 // 4.1
 // Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis, no
 // fim deve ser possível fazer o seguinte:
-var usuario = {
-  nome: 'Rocketseat',
-  endereco: {
-    cidade: 'Rio do Sul',
-    estado: 'SC'
-  }
-}; //resposta
 
-var nome = usuario.nome,
-    _usuario$endereco = usuario.endereco,
-    cidade = _usuario$endereco.cidade,
-    estado = _usuario$endereco.estado;
+const usuario = {
+    nome: 'Rocketseat',
+    endereco: {
+        cidade: 'Rio do Sul',
+        estado: 'SC',
+    }
+};
+
+//resposta
+const {nome, endereco:{cidade, estado}} = usuario
+
 console.log(nome); // Rocketseat
-
 console.log(cidade); // Rio do Sul
-
 console.log(estado); // SC
+
 // 4.2...
+
 // Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis, no
 // fim deve ser possível fazer o seguinte:
 //Utilize a desestruturação nos parâmetros da função para buscar o nome e idade do usuário
 // separadamente e a função poder retornar apenas:
 // return `${nome} tem ${idade} anos.`;
+
 //
 // function mostraInfo(usuario) {
 //     return `${usuario.nome} tem ${usuario.idade} anos.`;
 // }
 // mostraInfo({ nome: 'Diego', idade: 23 })
 
-function mostraInfo(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  console.log("".concat(nome, " tem ").concat(idade, " anos."));
-}
 
-mostraInfo({
-  nome: 'Diego',
-  idade: 23
-});
+function mostraInfo({nome, idade}) {
+    console.log(`${nome} tem ${idade} anos.`);
+}
+mostraInfo({ nome: 'Diego', idade: 23 })
